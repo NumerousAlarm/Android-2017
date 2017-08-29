bdcf4886288c621e5e208ef871b35e9ffa5f9031
Duke <ian.g12@hotmail.com>
2017-08-29 09:16:54 -0600

    Aplicacion de barbacha

--- Practica4/app/src/main/java/com/example/duke/practica4/MainActivity.java
+++ Practica4/app/src/main/java/com/example/duke/practica4/MainActivity.java
@@ -10,8 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast toast = Toast.makeText(this,"cadena",Toast.LENGTH_LONG);
-        toast.show();
+
+        int precio = 40;
+        int cantidad = 3;
+        int resultado=precio*cantidad;
+        if (resultado>80){
+            Toast toast = Toast.makeText(getApplicationContext(),"Soda gratis perro",Toast.LENGTH_LONG);
+            toast.show();
+        }else {
+            Toast toast = Toast.makeText(getApplicationContext(), "No tienes soda gratis carnal",Toast.LENGTH_LONG);
+            toast.show();
+        }
 
     }
 
