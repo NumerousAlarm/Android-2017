ec4b573c564fcab3d31b1dccd13b0c71f63dde98
Duke <ian.g12@hotmail.com>
2017-11-07 09:21:12 -0700

    Notificaciones

---
+++ Notificaciones/app/src/main/java/com/example/duke/notificaciones/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.duke.notificaciones;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.e("token", FirebaseInstanceId.getInstance().getToken());
+    }
+}
